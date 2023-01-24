211aa05a657144ba7f996ec2af7e82255f7dc892
Danny Harpigny <dannyhpy@disroot.org>
2023-01-24 22:08:07 +0100

    Support fetch API

--- duotree.user.js
+++ duotree.user.js
@@ -4,14 +4,52 @@
 // @match       https://*.duolingo.com/*
 // @grant       unsafeWindow
 // @run-at      document-start
-// @version     0.1.0
+// @version     0.2.0
 // @author      Danny Hpy
 // @description Restores the tree in Duolingo
 // ==/UserScript==
 
 const base = {
+  fetch: unsafeWindow.fetch,
   XMLHttpRequest: unsafeWindow.XMLHttpRequest
 }
+
+unsafeWindow.fetch = new Proxy(base.fetch, {
+  apply (target, thisArg, argumentList) {
+    return target.apply(thisArg, argumentList).then((res) => {
+      const url = argumentList[0]
+      if (typeof url !== 'string') return res
+      return new Proxy(res, {
+        get (target2, prop, receiver) {
+          if (typeof target2[prop] === 'function') {
+            if (prop === 'text') {
+              return new Proxy(target2[prop], {
+                apply (target3, thisArg2, argumentList2) {
+                  return target3.apply(target2, argumentList2).then((body) => {
+                    if (url.match(/\/2017-06-30\/users\/\d+/)) {
+                      try {
+                        const json = JSON.parse(body)
+                        if ('currentCourse' in json) {
+                          delete json.currentCourse.path
+                        }
+                        return JSON.stringify(json)
+                      } catch (err) {
+                        return body
+                      }
+                    }
+                    return body
+                  })
+                }
+              })
+            }
+            return target2[prop].bind(target2)
+          }
+          return target2[prop]
+        }
+      })
+    })
+  }
+})
 
 unsafeWindow.XMLHttpRequest = new Proxy(base.XMLHttpRequest, {
   construct (target) {
--- webext/pageScript.js
+++ webext/pageScript.js
@@ -1,6 +1,44 @@
 const base = {
+  fetch: unsafeWinow.fetch,
   XMLHttpRequest: window.XMLHttpRequest
 }
+
+window.fetch = new Proxy(base.fetch, {
+  apply (target, thisArg, argumentList) {
+    return target.apply(thisArg, argumentList).then((res) => {
+      const url = argumentList[0]
+      if (typeof url !== 'string') return res
+      return new Proxy(res, {
+        get (target2, prop, receiver) {
+          if (typeof target2[prop] === 'function') {
+            if (prop === 'text') {
+              return new Proxy(target2[prop], {
+                apply (target3, thisArg2, argumentList2) {
+                  return target3.apply(target2, argumentList2).then((body) => {
+                    if (url.match(/\/2017-06-30\/users\/\d+/)) {
+                      try {
+                        const json = JSON.parse(body)
+                        if ('currentCourse' in json) {
+                          delete json.currentCourse.path
+                        }
+                        return JSON.stringify(json)
+                      } catch (err) {
+                        return body
+                      }
+                    }
+                    return body
+                  })
+                }
+              })
+            }
+            return target2[prop].bind(target2)
+          }
+          return target2[prop]
+        }
+      })
+    })
+  }
+})
 
 window.XMLHttpRequest = new Proxy(base.XMLHttpRequest, {
   construct (target) {
